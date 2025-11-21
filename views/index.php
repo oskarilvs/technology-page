3b3183835aa33d483ea4ec274776bf8578a85036
Oskar Ilves <oskar.ilves@tptlive.ee>
2025-11-21 15:51:06 +0200

    Add dynamic posts

--- views/index.php
+++ views/index.php
@@ -1,3 +1,13 @@
+<?php
+  $title = 'World news';
+  $posts = [
+    ['title' => 'World news 1', 'author' => 'Pets', 'published' => '12.09.2025', 'body' => 'Some world news 1 body'],
+    ['title' => 'World news 2', 'author' => 'Maali', 'published' => '11.09.2025', 'body' => 'Some world news 2 body'],
+    ['title' => 'World news 3', 'author' => 'Annika', 'published' => '10.09.2025', 'body' => 'Some world news 3 body'],
+    ['title' => 'World news 4', 'author' => 'Mats', 'published' => '09.09.2025', 'body' => 'Some world news 4 body'],
+  ];
+?>
+
 <?php include __DIR__ . '/partials/header.php'; ?>
     <main class="container">
       <?php include __DIR__ . '/partials/hero.php'; ?>
--- views/us.php
+++ views/us.php
@@ -1,3 +1,13 @@
+<?php
+  $title = 'U.S news';
+  $posts = [
+    ['title' => 'U.S news 1', 'author' => 'Pets', 'published' => '12.09.2025', 'body' => 'Some U.S news 1 body'],
+    ['title' => 'U.S news 2', 'author' => 'Maali', 'published' => '11.09.2025', 'body' => 'Some U.S news 2 body'],
+    ['title' => 'U.S news 3', 'author' => 'Annika', 'published' => '10.09.2025', 'body' => 'Some U.S news 3 body'],
+    ['title' => 'U.S news 4', 'author' => 'Mats', 'published' => '09.09.2025', 'body' => 'Some U.S news 4 body'],
+  ];
+?>
+
 <?php include __DIR__ . '/partials/header.php'; ?>
     <main class="container">
       <div class="row g-5">
